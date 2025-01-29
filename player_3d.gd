9f668bd42dcc2ddd7f459165919236a0eef802d3
Nolan Denson-Childers <densonchilders123@gmail.com>
2025-01-29 09:40:53 -0600

    1/29/2025

--- player_3d.gd
+++ player_3d.gd
@@ -1,13 +1,18 @@
 extends CharacterBody3D
 
-
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+const WALK_SPEED = 5.0
+const SPRINT_SPEED = 8.5
+var SPEED = WALK_SPEED
+const JUMP_VELOCITY = 6.0
 
 const CAM_SENSITIVITY = 0.03
 @onready var camera = $Head/Camera3D
-
+@onready var camera_arm = $SpringArm3D
+@onready var camera_pos = camera.position
+@onready var BASE_FOV = camera.fov
 var first_person = true
+
+var FOV_CHANGE = 1.0
 
 const BOB_FREQ = 2.4 #how frequent the bobings are
 const BOB_AMP = 0.08 #how large the bobings are
@@ -22,6 +27,11 @@ func _unhandled_input(event: InputEvent) -> void:
 			self.rotate_y(-event.relative.x*(CAM_SENSITIVITY/10.0))
 			camera.rotate_x(-event.relative.y*(CAM_SENSITIVITY/10.0))
 			camera.rotation.x = clamp(camera.rotation.x,deg_to_rad(-40), deg_to_rad(60))
+		else:
+			self.rotate_y(-event.relative.x*(CAM_SENSITIVITY/10.0))
+			camera_arm.rotate_x(-event.relative.y*(CAM_SENSITIVITY/10.0))
+			camera_arm.rotation.x = clamp(camera_arm.rotation.x,deg_to_rad(-75), deg_to_rad(75))
+		
 	pass
 
 func _physics_process(delta: float) -> void:
@@ -30,8 +40,17 @@ func _physics_process(delta: float) -> void:
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		else: 
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED) 
-	
+	if Input.is_action_just_pressed("change_camera"): 
+		toggle_camera_parent()
 	# Add the gravity.
+	
+	if Input.is_action_just_pressed("sprint"):
+		SPEED = SPRINT_SPEED
+		FOV_CHANGE = 9.0
+	else:
+		SPEED = WALK_SPEED
+		FOV_CHANGE = 1.0
+	
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
@@ -50,6 +69,10 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 	
+	var velocity_clamped = clamp(velocity.length(), 0.5, SPEED * 2)
+	var target_fov = BASE_FOV + FOV_CHANGE * velocity_clamped
+	camera.fov = lerp(camera.fov, target_fov, delta * 8.0)
+	
 	t_bob += delta * velocity.length() * float(is_on_floor())
 	#a true boolen value can = 1 and a false can equal 
 	#the number value 0.
@@ -58,6 +81,20 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 
+func toggle_camera_parent():
+	var parent = "Head"
+	if first_person:
+		parent = "SpringArm3D"
+		#TODO: Model visibel
+	var child = camera
+	child.get_parent().remove_child(child)
+	get_node(parent).add_child(child)
+	camera = child 
+	if not first_person:
+		camera.position = camera_pos
+		#TODO: Model invisible
+	first_person = not first_person
+	
 func headbob(time):
 	var pos = Vector3.ZERO
 	pos.x = cos(time * BOB_FREQ / 2) * BOB_AMP
