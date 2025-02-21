d201fae78e079bdb784d2d55ba88bb1c703f3f40
Nolan Denson-Childers <densonchilders123@gmail.com>
2025-02-21 10:35:30 -0600

    2/21/2025-Update

---
+++ calibur_enemy.gd
@@ -0,0 +1,56 @@
+extends CharacterBody3D
+
+@onready var atk_area = $AttackArea
+@onready var dmg_area = $DamageArea
+@onready var nav_agent = $NavigationAgent3D
+@onready var animator = $Sketchfab_Scene/AnimationPlayer
+#var HEALTH = 1
+var SPEED = 3.0
+var ACCEL = 20
+var ATTACK = 10
+var KNOCKBACK = 16.0
+var WALKSPEED = SPEED
+var RUNSPEED = SPEED * 1.5
+
+
+func take_damage(_dmg):
+	animator.play("Death")
+	self.queue_free()
+
+
+func _physics_process(delta: float) -> void:
+	for player in get_tree().get_nodes_in_group("Player"):
+		if $AttackRange.overlaps_body(player):
+			nav_agent.target_position = player.global_position
+			SPEED = RUNSPEED
+		else:
+			SPEED = WALKSPEED
+		if atk_area.overlaps_body(player):
+			animator.play("Attack1")
+			player.take_damage(ATTACK)
+			player.inertia = (player.global_position-self.global_position) \
+							  .normalized() * KNOCKBACK
+			await animator.animation_finished
+	var dir = (nav_agent.target_position-self.global_position)
+	dir.y = 0
+	if dir.length() > 0.01:
+		var rot_angle = atan2(dir.x, dir.z)
+		self.rotation.y = lerp_angle(rotation.y, rot_angle, 5 * delta)
+	velocity = velocity.lerp(dir.normalized() * SPEED, ACCEL * delta)
+	
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	if animator.current_animation != "Attack1":
+		if velocity.length() <= 1:
+			animator.play("Idle")
+		elif SPEED == WALKSPEED:
+			animator.play("Walk")
+		elif SPEED == RUNSPEED:
+			animator.play("Walk")
+	
+	move_and_slide()
+
+
+func _ready():
+	nav_agent.target_position = global_position
--- enemy_3d.gd
+++ enemy_3d.gd
@@ -4,10 +4,13 @@ extends CharacterBody3D
 @onready var dmg_area: Area3D = $DamageArea
 @onready var nav_agent: NavigationAgent3D = $NavigationAgent3D
 
+var HEALTH = 200
 var SPEED = 3.0
 var ACCEL = 20
 var ATTACK = 10
 var KNOCKBACK = 16.0
+var WALKSPEED = SPEED
+var RUNSPEED = SPEED * 1.5
 
 func take_damage(_dmg):
 	self.queue_free()
@@ -15,14 +18,17 @@ func take_damage(_dmg):
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
 	for player in get_tree().get_nodes_in_group("player"):
 		if $AttackRange.overlaps_body(player):
 			nav_agent.target_position = player.global_position
+			SPEED = RUNSPEED
 		if atk_area.overlaps_body(player):
 			player.take_damage(ATTACK)
 			player.inertia = (player.global_position-global_position).normalized()*KNOCKBACK
 	var dir = (nav_agent.target_position-self.global_position).normalized()
 	velocity = velocity.lerp(dir * SPEED, ACCEL * delta)
+	
 	move_and_slide()
 	
 func _ready():
