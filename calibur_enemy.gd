6250f249a2a26a81550b987c540ff442503052ce
Nolan Denson-Childers <densonchilders123@gmail.com>
2025-03-04 10:32:51 -0600

    3/4/2025-Update

--- calibur_enemy.gd
+++ calibur_enemy.gd
@@ -1,9 +1,12 @@
 extends CharacterBody3D
 
-@onready var atk_area = $AttackArea_stab
-@onready var dmg_area = $DamageArea
+@onready var hit_area = $"Sketchfab_Scene/Sketchfab_model/470fbca5ee3345d09f1c05d26df614bd_fbx/Object_2/RootNode/Armature/Object_8/Skeleton3D/Object_15/HitArea"
 @onready var nav_agent = $NavigationAgent3D
 @onready var animator = $Sketchfab_Scene/AnimationPlayer
+@onready var dmg_area = $DamageArea
+@onready var atk_jump = $AttackArea_jump
+@onready var atk_stab = $AttackArea_stab
+@onready var atk_stomp = $AttackArea_stomp
 #var HEALTH = 1
 var SPEED = 10.0
 var ACCEL = 20
@@ -24,7 +27,12 @@ func _physics_process(delta: float) -> void:
 			SPEED = RUNSPEED
 		else:
 			SPEED = WALKSPEED
-		if atk_area.overlaps_body(player):
+		if atk_stab.overlaps_body(player):
+			nav_agent.target_position = $".".global_position
+			animator.play("Attack1")
+			
+			
+		if hit_area.overlaps_body(player):
 			animator.play("Attack1")
 			player.take_damage(ATTACK)
 			player.inertia = (player.global_position-self.global_position).normalized() * KNOCKBACK
